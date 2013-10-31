a9017b4cebe0caf7fbcf13258415c8efe6de78f0
Ryan Nelson <rnelsonchem@gmail.com>
2013-10-31 15:11:38 -0400

    Simplify and rewrite
    Simplified and rewrote a large chunk of Sally's code.
    
    I also made it a bit more general purpose by adding several new keywords
    to the function. The default behavior should be the same as before.
    
    One backward incompatibility. Did change the name of the ratios columns.

--- sally.py
+++ sally.py
@@ -1,58 +1,61 @@
 import re
 import pandas as pds
-#import numpy as np
 
-def creatingDataFrames(file_name, total_cols=30):
+def creatingDataFrames(file_name, total_cols=30, index_col='Formula',
+                       formula_col='Formula', 
+                       ratiolist=[('O', 'C'), ('H', 'C')]
+                       ):
     csv = open(file_name)
-    csv.next()
-    csv.next()
-
-    header = csv.next()
-    header = header.split(',')
-
+    
     new_list = []
-    formulas = []
-    #total_cols  = 30
+    header = None
 
     for line in csv:
-        if line.strip() == '': continue
+        # Skip comment lines
+        if line[0] == '#': 
+            continue
+        # Skip blank lines
+        elif line.isspace(): 
+            continue
+        # Make a header list from the very first line
+        elif header == None:
+            header = line.split(',')
+            continue                  
+        # Process the rest of the data
         sp = line.split(',')
-        if sp[4] == '': continue
-        formulas.append(sp.pop(4))
-        new_list.append(sp[:total_cols])
-        
+        new_list.append( sp[:total_cols] )
+    
     header = header[:total_cols]
 
-    df = pds.DataFrame(new_list, index = formulas, columns = header)
+    df = pds.DataFrame(new_list, columns=header) 
+    if formula_col != None:
+        table = pds.DataFrame( ratiocalc(df[formula_col], ratiolist) )    
+        df = df.join(table)
+
+    return df.set_index(index_col)
     
-    Ratios = []
+def ratiocalc(formulas, ratiolist):
+    expression = r'([A-Za-z]{1,2})(\d{0,2})'
+    regex = re.compile( expression )
     
+    ratios = []    
     for line in formulas:
-        expression = r'([A-Za-z]{1,2})(\d{0,2})'
-        match = re.findall(expression, line)
-        
         match_dict = {}
+        
+        match = regex.findall(line)
+        
         for atom, num in match:
             if num == '':
                 match_dict[atom] = 1.
             else:
                 match_dict[atom] = float(num)
-        if 'O' in match_dict and 'C' in match_dict:
-            match_dict['OCratio'] = match_dict['O'] / match_dict ['C']
-        if 'H' in match_dict and 'C' in match_dict:
-            match_dict['HCratio'] = match_dict['H'] / match_dict['C']
-        Ratios.append(match_dict)
-
-    Table = pds.DataFrame(Ratios, index = formulas)
-
-    df = df.join(Table)
-
-    return df
-    
-    #x = df['OCratio']
-    #y = df['HCratio']
-    #z1 = df['Volume']
-    #z = array(z1, dtype=float)
-    
-    #plt.scatter(x,y,s=z*50,c=z)
-    #plt.colorbar()
+                
+        for atomA, atomB in ratiolist:        
+            if atomA in match_dict and atomB in match_dict:
+                column_name = atomA + atomB + ' ratio'
+                ratio = match_dict[atomA]/match_dict[atomB]
+                match_dict[column_name] = ratio
+        
+        ratios.append(match_dict)
+        
+    return ratios
